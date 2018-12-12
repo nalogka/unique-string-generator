d422312e5fb0d49176391f60b7ef9180ef824376
Anton Tyutin <anton@tyutin.ru>
2018-12-12 17:58:30 +0300

    Документирование кода через phpdoc

--- src/Converter.php
+++ src/Converter.php
@@ -5,10 +5,50 @@ namespace Nalogka\UniqueStringGenerator;
 class Converter
 {
     /**
-     * Приведение строки с известным набором символов к десятичному представлению
-     * @param string $charset
-     * @param string $string
-     * @return string
+     * Кодирование (упаковка) строки с известным набором символов в бинарную строку
+     * @param string $charset Набор символов алфавита, используемый в кодируемой строке 
+     * @param string $string  Строка для кодирования
+     * @return string Бинарная строка
+     */
+    public static function toBinary($charset, $string)
+    {
+        $decimalStr = self::toDecimal($charset, $string);
+
+        if ($decimalStr === '0') {
+            return "\x0";
+        }
+        
+        $result = '';
+        while ($decimalStr !== '0') {
+            $result = pack('C', bcmod($decimalStr, 256)) . $result;
+            $decimalStr = bcdiv($decimalStr, 256);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Восстановления строки с известным набором символов из бинарной строки
+     * @param string $charset Алфавит, используемый в востанавливаемой строке
+     * @param string $decimalStr Строка в сыром (бинарном, хранимом в слое персистентности) виде. 
+     * @param integer $length Длина восстанавливаемой строки. Если восстановленная строка меньше этого аргумента, то она дополняется слева первым символом алфавита.
+     * @return string Строка из символов исходного алфавита
+     */
+    public static function fromBinary($charset, $binaryString, $length)
+    {
+        $decimalStr = '0';
+        for ($i = 0, $lastPos = strlen($binaryString) - 1; $i <= $lastPos; $i++) {
+            $decimalStr = bcadd($decimalStr, bcmul(unpack('C', $binaryString[$lastPos - $i])[1], bcpow(256, $i)));
+        }
+
+        return self::fromDecimal($charset, $decimalStr, $length);
+    }
+
+    /**
+     * Кодирование строки с указанным алфавитом в строку, состоящую из цифр [0-9]
+     * @param string $charset Набор символов алфавита, используемый в кодируемой строке 
+     * @param string $string  Строка для кодирования 
+     * @return string Строка из цифр [0-9]
      */
     public static function toDecimal($charset, $string)
     {
@@ -35,10 +75,11 @@ class Converter
     }
 
     /**
-     * Приведение строки из десятичного представления к строке с известным набором символов
-     * @param string $charset
-     * @param string $string
-     * @return int|string
+     * Восстановление строки с указанным алфавитом из строки, состоящей из цифр [0-9]
+     * @param string $charset Алфавит, используемый в востанавливаемой строке
+     * @param string $decimalStr Строка из цифр [0-9]
+     * @param integer $length Длина восстанавливаемой строки. Если восстановленная строка меньше этого аргумента, то она дополняется слева первым символом алфавита.
+     * @return string Строка из символов исходного алфавита
      */
     public static function fromDecimal($charset, $decimalStr, $length)
     {
@@ -53,43 +94,5 @@ class Converter
         }
 
         return str_pad($string, $length, $charset[0], STR_PAD_LEFT);
-    }
-
-    /**
-     * Упаковка строки с известным набором символов в бинарную строку
-     * @param string $charset Набор символов алфавита 
-     * @param string $string  Строка для преобразования
-     * @return string
-     */
-    public static function toBinary($charset, $string)
-    {
-        $decimalStr = self::toDecimal($charset, $string);
-
-        if ($decimalStr === '0') {
-            return "\x0";
-        }
-        
-        $result = '';
-        while ($decimalStr !== '0') {
-            $result = pack('C', bcmod($decimalStr, 256)) . $result;
-            $decimalStr = bcdiv($decimalStr, 256);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Восстановления строки с известным набором символов из бинарной строки
-     * @param string $binaryString
-     * @return string
-     */
-    public static function fromBinary($charset, $binaryString, $length)
-    {
-        $decimalStr = '0';
-        for ($i = 0, $lastPos = strlen($binaryString) - 1; $i <= $lastPos; $i++) {
-            $decimalStr = bcadd($decimalStr, bcmul(unpack('C', $binaryString[$lastPos - $i])[1], bcpow(256, $i)));
-        }
-
-        return self::fromDecimal($charset, $decimalStr, $length);
     }
 }
